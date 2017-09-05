01f284eefad444cc3f9113a7875ceb37168b8b63
Alex Botello <alexander.botello@g.austincc.edu>
2017-09-05 17:38:53 -0500

    Auto remove outdated concerts from database

--- app/commands/concert.py
+++ app/commands/concert.py
@@ -1,53 +1,59 @@
-from discord.ext import commands
-from commands.utils.formatter import block
-from commands.utils import checks
-from config import logger
+import datetime
+import json
+import requests
 import pymongo
 import discord
-import requests
 import asyncio
-import json
-import time
 import settings
+from config import logger
+from discord.ext import commands
+from commands.utils import checks
+from commands.utils.formatter import block
 
 
 class Concerts:
     """Concert Display Commands"""
-    # TODO AUTO CLEAN THE DATABASE TO REMOVE CONCERT DATES THAT HAVE ALREADY HAPPENED
-    # TODO REMOVE ARTISTS COMMAND
     def __init__(self, bot):
         self.bot = bot
         self.api = '2.0'
-        self._client = pymongo.MongoClient(settings.DB_URL)
+        self._client = None
         self._db = None
         self._artists = None
         self._radius = settings.RADIUS
         self._location = settings.LOCATION
         self.id = settings.ID
         self._url = settings.BIT_URL
+        self.date = datetime.datetime.now().isoformat()
  
+    def connect_client(self):
+        try:
+            return pymongo.MongoClient(settings.DB_URL)
+        except pymongo.errors.ConnectionFailure:
+            logger.error("Could not connect to MongoDB...")
+
     def connect_database(self, server):
+        self._client = self.connect_client()
         db = self._client.setting.DB_NAME
         return db[server.name]
     
     def load_artists(self, server):
         self._db = self.connect_database(server)
-        query = self._db.find_one({"id": server.id})
-        if query:
-            self._client.close()
-            return [a for a in query['artists']]
-        else:
-            self._client.close()
-            return None
-    
-    def _verify_database_integrity(self):
-        pass
-
+        try:
+            query = self._db.find_one({"id": server.id})
+            if query:
+                self._client.close()
+                return [a for a in query['artists']]
+            else:
+                self._client.close()
+                return None
+        except pymongo.errors.OperationFailure:
+            logger.error("Could not retrieve artists from database..")
+        
     async def concert_finder(self, ctx):
         """Loop that scrapes concert data"""
         channel = ctx.message.channel
         while not self.bot.is_closed:
-            all_concerts = self.find_all_concerts(ctx)
+            all_concerts = await self.find_all_concerts(ctx)
             for concert in all_concerts:
                 image = discord.Embed(colour=discord.Colour.default())
                 image.set_image(url=concert['image'])
@@ -57,12 +63,12 @@ class Concerts:
             #await asyncio.sleep(60 * 500)
             await asyncio.sleep(30)
     
-    def find_all_concerts(self, ctx):
+    async def find_all_concerts(self, ctx):
         """
         Use BandsinTown API to search for concert events
         """
         if self._artists is None:
-            self._artists = self.load_artists(ctx.message.server)
+            await self.bot.say("Please add artists first using '?concert add <artists>'")
         if self._db is None:
             self._db = self.connect_database(ctx.message.server)
        
@@ -78,7 +84,6 @@ class Concerts:
             resp = requests.get(URL, params=param)
             if resp.status_code != 200:
                 raise ValueError(f'{resp.status_code} response code')
-            # Load JSON data from response
             json_data = json.loads(resp.text)
 
             old_result_found = 0
@@ -86,18 +91,28 @@ class Concerts:
             for event in json_data:
                 concert = concerts.find_one({'_id': event['id']})
                 if concert:
-                    old_result_found += 1
+                    if concert['datetime'] < self.date:
+                        try:
+                            concerts.delete_one(concert)
+                        except pymongo.errors.OperationFailure:
+                            logger.error('Error removing outdated concert from database...')
+                    else:
+                        old_result_found += 1
                 else:
                     concert = {
                         "_id": event['id'],
                         "title": event['title'],
                         "date": event['formatted_datetime'],
                         'artist': event['artists'][0]['name'],
-                        'image': event['artists'][0]['image_url']
+                        'image': event['artists'][0]['image_url'],
+                        'datetime': event['datetime']
                     }
-                    concerts.insert_one(concert)
-                    new_results_found += 1
-                    results.append(concert)
+                    try:
+                        concerts.insert_one(concert)
+                        new_results_found += 1
+                        results.append(concert)
+                    except pymongo.errors.OperationFailure:
+                        logger.error('Error inserting concert into database...')
 
             if json_data == []:
                 logger.info(f'Found No Results For {artist}')
@@ -119,6 +134,7 @@ class Concerts:
     async def start(self, ctx):
         """Start Concert Finder"""
         server = ctx.message.server
+        self._artists = self.load_artists(server)
         if self._db is None:
             self._db = self.connect_database(server)
         await self.concert_finder(ctx)
@@ -140,7 +156,7 @@ class Concerts:
         if self._db is None:
             self._db = self.connect_database(guild)
         
-        #TODO TURN THIS INTO LIST COMPREHENSION OR CONDENSE IT 
+        #TODO TURN THIS INTO LIST COMPREHENSION OR CONDENSE IT
         for artist in artists:
             artist = artist.replace("_", " ")
             try:
@@ -188,9 +204,9 @@ class Concerts:
     
     @concert.command(pass_context=True, hidden=True)
     @checks.is_owner()
-    async def radius(self, ctx, radius):
+    async def radius(self, ctx, radius: int):
         """Changes the search radius"""
-        self._radius = radius
+        self._radius = str(radius) if radius <= 150 else '150'
         await self.bot.say(f"Radius setting has been changed to {self._radius}")
         
     @concert.command(pass_context=True, name="list")
